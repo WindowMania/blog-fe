fc5350e1723a0687a72c48503b41a98eea8fa34a
kyb <kybdev@gmail.com>
2023-01-14 20:44:59 +0900

    <:feat> 글 작성 하기

--- libs/RestApi.ts
+++ libs/RestApi.ts
@@ -5,14 +5,23 @@ export interface RestResponse {
 }
 
 export class RestApi {
-    constructor() {}
+    constructor() {
+    }
+
+    private getHeader(opt: any): HeadersInit {
+        const headers: HeadersInit = {
+            "Content-Type": "application/json",
+        }
+        if (opt && opt.accessKey) {
+            headers['Authorization'] = 'Bearer ' + opt.accessKey
+        }
+        return headers
+    }
 
     public async post(url: string, data: any, opt?: any): Promise<BasicRestResponse> {
         const res = await fetch(url, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
+            headers: this.getHeader(opt),
             body: JSON.stringify(data),
         })
         const res_data = await res.json()
--- organism/PostCreator.tsx
+++ organism/PostCreator.tsx
@@ -1,17 +1,22 @@
 import PostEditMolecule from "@/components/molecule/post-editor"
 import {useCallback} from "react";
+import restApi from "@/libs/RestApi";
+import useLogin from "@/hooks/useLogin";
 
-export interface Props {}
+export interface Props {
+}
 
 
 export default function PostCreator(props: Props) {
+    const {accessKey} = useLogin()
 
-    const submit = useCallback(async (ctx:PostEditorModel) => {
-        const ret:BasicRestResponse = {
-            ok:true
-        }
-        return Promise.resolve(ret)
-    }, [])
+    const submit = useCallback(async (ctx: PostEditorModel) => {
+        const url = "http://localhost:8000/api/v1/post"
+        console.log("어세스키?", accessKey)
+        const res = await restApi.post(url, {...ctx}, {accessKey})
+        console.log("봅시다..", res)
+        return Promise.resolve(res)
+    }, [accessKey])
 
     return (
         <PostEditMolecule mode={'create'} onSubmit={submit}/>
